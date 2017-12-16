795596fb361c59ae0ff155824931b88ae7d6f19e
Jean-Roger <jeanroger.orsini@gmail.com>
2017-12-16 14:15:02 +0900

    Added some minor changes will pick up on that one later.

--- routes/index.js
+++ routes/index.js
@@ -16,34 +16,35 @@ request('http://boxrec.com/en/boxer/' + boxer_id, (err, res, html) => {
   const $ = cheerio.load(html)
   if(!err) {
     $('.dataTable tr').map((e, i) => {
-      setTimeout(() => {
-        const date = $('.dataTable tr').eq(e).children().eq(1).text().trim()
-        if(/\d/g.test(date)) {
-          const url = 'http://boxrec.com' + $('.dataTable tr').eq(e).children().eq(10).children().eq(0).children().eq(1).prop('href');
+      const date = $('.dataTable tr').eq(e).children().eq(1).text().trim()
+      if(/\d/g.test(date)) {
+        const url = 'http://boxrec.com' + $('.dataTable tr').eq(e).children().eq(10).children().eq(0).children().eq(1).prop('href');
+        
+        request(url, (err, res, body) => {
+          const jQuery = cheerio.load(body);
+
           fight = {
             fight_date: date,
             opponent_name: $('.dataTable tr').eq(e).children().eq(3).text().replace(/\d/g,'').trim(),
             opponent_stats: $('.dataTable tr').eq(e).children().eq(3).text().replace(/[A-Za-z\n]/g,'').trim(),
             result: $('.dataTable tr').eq(e).children().eq(7).text().replace('\n','').trim(),
             score: $('.dataTable tr').eq(e).children().eq(8).text().replace(/[\n\/\d]/g,'').trim(),
-            round: $('.dataTable tr').eq(e).children().eq(8).text().replace(/[\nA-Z]/g,'').trim()
+            round: $('.dataTable tr').eq(e).children().eq(8).text().replace(/[\nA-Z]/g,'').trim(),
+            points: jQuery('.responseLessDataTable tr').eq(12).text().trim().split(' ')[0].replace(/\D/g,''),
+            age: jQuery('.responseLessDataTable tr').eq(14).text().trim().split(' ')[0].replace(/\D/g,'')
           };
-          request(url, (err, res, body) => {
-            const jQuery = cheerio.load(body)
-            fight.points = jQuery('.responseLessDataTable tr').eq(12).text().trim().split(' ')[0].replace(/\D/g,'');
-            fight.age = jQuery('.responseLessDataTable tr').eq(14).text().trim().split(' ')[0].replace(/\D/g,'')
-            boxer_data.fight_list.push(fight)
-            data = '[' + JSON.stringify(boxer_data)+ ']';
-            if(data.length > 50) {
-              fs.writeFile('public/data/' + boxer_id + '.json', data , err => {
-                if(!err) {
-                  console.log('File for boxer ' + boxer_id + ' saved!')
-                }
-              })
-            }
-          })
-        }
-      }, 2000);
+
+          boxer_data.fight_list.push(fight);
+          data = '[' + JSON.stringify(boxer_data)+ ']';
+          if(data.length > 50) {
+            fs.writeFile('public/data/' + boxer_id + '.json', data , err => {
+              if(!err) {
+                console.log('File for boxer ' + boxer_id + ' saved!')
+              }
+            })
+          }
+        })
+      }
     });
   }
 });
