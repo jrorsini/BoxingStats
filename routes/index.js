2321058a994ff8a4f62e518529adf042d4fb0e80
Jean-Roger <jeanroger.orsini@gmail.com>
2017-12-16 22:33:06 +0900

    Added a scrapping function to use in the console tool.

--- routes/index.js
+++ routes/index.js
@@ -12,42 +12,25 @@ let boxer_data = {
 let fight;
 let data;
 
-request('http://boxrec.com/en/boxer/' + boxer_id, (err, res, html) => {
-  const $ = cheerio.load(html)
-  if(!err) {
-    $('.dataTable tr').map((e, i) => {
-      const date = $('.dataTable tr').eq(e).children().eq(1).text().trim()
-      if(/\d/g.test(date)) {
-        const url = 'http://boxrec.com' + $('.dataTable tr').eq(e).children().eq(10).children().eq(0).children().eq(1).prop('href');
-        
-        request(url, (err, res, body) => {
-          const jQuery = cheerio.load(body);
+let fight_list = () => {
+  var fight_list = [];
+  $('.dataTable .drawRowBorder').map((e, i) => {
+      const points = $('.dataTable .drawRowBorder').eq(e).children().eq(3).text().trim().split('➞')[1]
+      fight = {
+        fight_date: $('.dataTable .drawRowBorder').eq(e).children().eq(1).text().trim(),
+        weight: $('.dataTable .drawRowBorder').eq(e).children().eq(2).text().trim(),
+        opponent_name: $('.dataTable .drawRowBorder').eq(e).children().eq(5).text().replace(/\d/g,'').trim(),
+        opponent_stats: $('.dataTable .drawRowBorder').eq(e).children().eq(5).text().replace(/[A-Za-z\n]/g,'').trim().split(/\s/g),
+        result: $('.dataTable .drawRowBorder').eq(e).children().eq(11).text().trim(),
+        score: $('.dataTable .drawRowBorder').eq(e).children().eq(12).text().replace(/[\n\/\d]/g,'').trim(),
+        round: $('.dataTable .drawRowBorder').eq(e).children().eq(12).text().replace(/[\nA-Z]/g,'').trim(),
+        points: Number(points.replace(',',''))
+      };
+      fight_list.push(fight);
+  });
+  console.table(fight_list);
+};
 
-          fight = {
-            fight_date: date,
-            opponent_name: $('.dataTable tr').eq(e).children().eq(3).text().replace(/\d/g,'').trim(),
-            opponent_stats: $('.dataTable tr').eq(e).children().eq(3).text().replace(/[A-Za-z\n]/g,'').trim(),
-            result: $('.dataTable tr').eq(e).children().eq(7).text().replace('\n','').trim(),
-            score: $('.dataTable tr').eq(e).children().eq(8).text().replace(/[\n\/\d]/g,'').trim(),
-            round: $('.dataTable tr').eq(e).children().eq(8).text().replace(/[\nA-Z]/g,'').trim(),
-            points: jQuery('.responseLessDataTable tr').eq(12).text().trim().split(' ')[0].replace(/\D/g,''),
-            age: jQuery('.responseLessDataTable tr').eq(14).text().trim().split(' ')[0].replace(/\D/g,'')
-          };
-
-          boxer_data.fight_list.push(fight);
-          data = '[' + JSON.stringify(boxer_data)+ ']';
-          if(data.length > 50) {
-            fs.writeFile('public/data/' + boxer_id + '.json', data , err => {
-              if(!err) {
-                console.log('File for boxer ' + boxer_id + ' saved!')
-              }
-            })
-          }
-        })
-      }
-    });
-  }
-});
 
 
 
