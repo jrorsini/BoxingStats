30610525a836a1a5495a2637efa4b449be6c57ff
Jean-Roger <jeanroger.orsini@gmail.com>
2017-12-16 14:09:45 +0900

    Need to fix that issue about points and age, as it just keeps the last one from the frist row.

--- routes/index.js
+++ routes/index.js
@@ -9,49 +9,42 @@ const boxer_id = 474;
 let boxer_data = {
   fight_list: []
 };
-let link_list = [];
 let fight;
 let data;
 
 request('http://boxrec.com/en/boxer/' + boxer_id, (err, res, html) => {
   const $ = cheerio.load(html)
   if(!err) {
-    let fight_date =  
-    $('.dataTable .mobileActions').map((e, i) => {
-      link_list.push($('.dataTable .mobileActions').eq(e).children().eq(2).prop('href'))
-    });
-    
-    link_list.map(e => {
-      request(e, (err, res, html) => {
-        const jQuery = cheerio.load(html);
-        $('.dataTable tr').map((e, i) => {
-          console.log(fight)
+    $('.dataTable tr').map((e, i) => {
+      setTimeout(() => {
+        const date = $('.dataTable tr').eq(e).children().eq(1).text().trim()
+        if(/\d/g.test(date)) {
+          const url = 'http://boxrec.com' + $('.dataTable tr').eq(e).children().eq(10).children().eq(0).children().eq(1).prop('href');
           fight = {
-            fight_date: $('.dataTable tr').eq(e).children().eq(1).text().trim(),
+            fight_date: date,
             opponent_name: $('.dataTable tr').eq(e).children().eq(3).text().replace(/\d/g,'').trim(),
             opponent_stats: $('.dataTable tr').eq(e).children().eq(3).text().replace(/[A-Za-z\n]/g,'').trim(),
             result: $('.dataTable tr').eq(e).children().eq(7).text().replace('\n','').trim(),
             score: $('.dataTable tr').eq(e).children().eq(8).text().replace(/[\n\/\d]/g,'').trim(),
-            round: $('.dataTable tr').eq(e).children().eq(8).text().replace(/[\nA-Z]/g,'').trim(),
-            points: jQuery('.responseLessDataTable tr').eq(12).text().trim().split(' ')[0].replace(/\D/g,''),
-            age: jQuery('.responseLessDataTable tr').eq(14).text().trim().split(' ')[0].replace(/\D/g,'')
+            round: $('.dataTable tr').eq(e).children().eq(8).text().replace(/[\nA-Z]/g,'').trim()
           };
-          boxer_data.fight_list.push(fight)
-        });
-        /*
-        setTimeout(() => {
-          data = '[' + JSON.stringify(boxer_data)+ ']';
-          if(data.length > 50) {
-            fs.writeFile('public/data/' + boxer_id + '.json', data , err => {
-              if(!err) {
-                console.log('File for boxer ' + boxer_id + ' saved!')
-              }
-            })
-          }
-        }, 1000);
-        */
-      });
-    })
+          request(url, (err, res, body) => {
+            const jQuery = cheerio.load(body)
+            fight.points = jQuery('.responseLessDataTable tr').eq(12).text().trim().split(' ')[0].replace(/\D/g,'');
+            fight.age = jQuery('.responseLessDataTable tr').eq(14).text().trim().split(' ')[0].replace(/\D/g,'')
+            boxer_data.fight_list.push(fight)
+            data = '[' + JSON.stringify(boxer_data)+ ']';
+            if(data.length > 50) {
+              fs.writeFile('public/data/' + boxer_id + '.json', data , err => {
+                if(!err) {
+                  console.log('File for boxer ' + boxer_id + ' saved!')
+                }
+              })
+            }
+          })
+        }
+      }, 2000);
+    });
   }
 });
 
