7f0450196cf3a3da6ad82c8ec28be998fbfa4309
jean-roger orsini <jean-roger@VR-MC040-3.local>
2017-12-18 11:45:21 +0900

    Remade the interface.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,13 +11,24 @@ let fillPts = () => {
     
     let markup = `
         <ul id="popularity">
-            ${data.fight_list.reverse().map(
-                e => `<li><span class="lvl fight--${e.result}" style="width:${e.points}px"></span><li>`
+            ${data.fight_list.map(
+                e => `<li>
+                    <span class="lvl fight--${e.result}" style="height:${e.points / max * 100}px"></span>
+                    <div class="figther_modal">
+                        <p>${e.fight_date}</p>
+                        <p>${e.opponent_name}</p>
+                        <p>
+                            <span>${e.opponent_stats[0]}</span>
+                            <span>${e.opponent_stats[1]}</span>
+                            <span>${e.opponent_stats[2]}</span>
+                        </p>
+                        <p>${e.result} by ${e.score} in round ${e.round}</p>
+                    </div>
+                </li>`
             ).join('')
             }
         </ul>
     `
-
     document.body.innerHTML = markup;
 }
 
