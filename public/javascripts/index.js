860cea3a91220c347d5cba4dadec364a765caddc
Jean-Roger <jeanroger.orsini@gmail.com>
2017-12-20 14:55:57 +0900

    Added minor changes.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -26,21 +26,23 @@ let fillPts = () => {
                     <div class="figther_modal">
                         <p>${e.fight_date}</p>
                         <div>
-                            <p><b>${data.name}</b> (${e.points})</p>
-                            <p class="opponent-s_records">
-                                <span class="opW">${e.result === 'W' ? wins += 1 : wins }</span>
-                                <span class="opL">${e.result === 'L' ? losses += 1 : losses }</span>
-                                <span class="opD">${e.result === 'D' ? draws += 1 : draws }</span>
-                            </p>
-                        </div>
-                        <p>VS.</p>
-                        <div>
-                            <p><b>${e.opponent_name}</b></p>
-                            <p class="opponent-s_records">
-                                <span class="opW">${e.opponent_stats[0]}</span>
-                                <span class="opL">${e.opponent_stats[1]}</span>
-                                <span class="opD">${e.opponent_stats[2]}</span>
-                            </p>
+                            <section>
+                                <p><b>${data.name}</b> (${e.points})</p>
+                                <p class="opponent-s_records">
+                                    <span class="opW">${e.result === 'W' ? wins += 1 : wins }</span>
+                                    <span class="opL">${e.result === 'L' ? losses += 1 : losses }</span>
+                                    <span class="opD">${e.result === 'D' ? draws += 1 : draws }</span>
+                                </p>
+                            </section>
+                            <p>VS.</p>
+                            <section>
+                                <p><b>${e.opponent_name}</b></p>
+                                <p class="opponent-s_records">
+                                    <span class="opW">${e.opponent_stats[0]}</span>
+                                    <span class="opL">${e.opponent_stats[1]}</span>
+                                    <span class="opD">${e.opponent_stats[2]}</span>
+                                </p>
+                            </section>
                         </div>
                         <p><b class="op${e.result}">${e.result === 'W' ? 'Won' : 'Lost'}</b> by <b>${e.score}</b> in round <b>${e.round}</b></p>
                     </div>
